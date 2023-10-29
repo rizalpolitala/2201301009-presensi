fd875ddb209903fae9b032622c224e4ce70ede96
Rizal <muhammad.rizali.rahman@mhs.politala.ac.id>
2023-10-29 21:43:26 +0800

    first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,247 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	private function _login()
+	{
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+
+		// jika usernya ada
+		if ($user) {
+			// jika usernya aktif
+			if ($user['is_active'] == 1) {
+				// cek password
+				if (password_verify($password, $user['password'])) {
+					$data = [
+						'email' => $user['email'],
+						'role_id' => $user['role_id']
+					];
+					$this->session->set_userdata($data);
+					if ($user['role_id'] == 1) {
+						redirect('admin');
+					} else {
+						redirect('user');
+					}
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
+					redirect('auth');
+				}
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+				redirect('auth');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
+			redirect('auth');
+		}
+	}
+
+
+	public function index()
+	{
+		if ($this->session->userdata('email')) {
+			redirect('user');
+		}
+
+
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'Login Page';
+			$this->load->view('_templates/auth_header', $data);
+			$this->load->view('auth/login');
+			$this->load->view('_templates/auth_footer');
+		} else {
+			// validasinya success
+			$this->_login();
+		}
+	}
+
+
+	public function register()
+	{
+		if ($this->session->userdata('email')) {
+			redirect('user');
+		}
+
+
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This email has already registered!'
+		]);
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+			'matches' => 'Password dont match!',
+			'min_length' => 'Password too short!'
+		]);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
+		if ($this->form_validation->run() == false) {
+
+
+			$data['title'] = 'Registration';
+
+
+			$this->load->view('_templates/auth_header', $data);
+			$this->load->view('auth/register');
+			$this->load->view('_templates/auth_footer');
+		} else {
+			$email = $this->input->post('email', true);
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name', true)),
+				'email' => htmlspecialchars($email),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 0,
+				'date_created' => time()
+			];
+
+
+			// siapkan token
+			$token = base64_encode(random_bytes(32));
+			$user_token = [
+				'email' => $email,
+				'token' => $token,
+				'date_created' => time()
+			];
+
+
+			$this->db->insert('user', $data);
+			$this->db->insert('user_token', $user_token);
+
+
+			$this->_sendEmail($token, 'verify');
+
+
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
+			redirect('auth');
+		}
+	}
+
+	private function _sendEmail($token, $type)
+	{
+		$config = [
+			'protocol'  => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'muhammad.rizali.rahman@mhs.politala.ac.id',
+			'smtp_pass' => 'lcwz gxgd byru djqv',
+			'smtp_port' => 465,
+			'mailtype'  => 'html',
+			'charset'   => 'utf-8',
+			'newline'   => "\r\n"
+		];
+
+
+		$this->email->initialize($config);
+
+
+		$this->email->from('isi dengan email Anda', 'Presensi');
+		$this->email->to($this->input->post('email'));
+
+
+		if ($type == 'verify') {
+			$this->email->subject('Account Verification');
+			$this->email->message('Click this link to verify you account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate</a>');
+		} else if ($type == 'forgot') {
+			$this->email->subject('Reset Password');
+			$this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+		}
+
+
+		if ($this->email->send()) {
+			return true;
+		} else {
+			echo $this->email->print_debugger();
+			die;
+		}
+	}
+
+	public function verify()
+	{
+		$email = $this->input->get('email');
+		$token = $this->input->get('token');
+
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+
+		if ($user) {
+			$user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+
+			if ($user_token) {
+				if (time() - $user_token['date_created'] < (60 * 60 * 24)) {
+					$this->db->set('is_active', 1);
+					$this->db->where('email', $email);
+					$this->db->update('user');
+
+
+					$this->db->delete('user_token', ['email' => $email]);
+
+
+					$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">' . $email . ' has been activated! Please login.</div>');
+					redirect('auth');
+				} else {
+					$this->db->delete('user', ['email' => $email]);
+					$this->db->delete('user_token', ['email' => $email]);
+
+
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Token expired.</div>');
+					redirect('auth');
+				}
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong token.</div>');
+				redirect('auth');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong email.</div>');
+			redirect('auth');
+		}
+	}
+
+
+	public function forgotpassword()
+	{
+		$this->load->view('_templates/auth_header');
+		$this->load->view('auth/forgotpassword');
+		$this->load->view('_templates/auth_footer');
+	}
+	
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
+        redirect(base_url('auth'));
+    }
+
+	public function blocked()
+    {
+        $data['title'] = '403 Akses Dibatasi';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('_templates/header', $data);
+        $this->load->view('_templates/navbar');
+        $this->load->view('_templates/sidebar', $data);
+        $this->load->view('auth/blocked');
+        $this->load->view('_templates/footer');
+    }
+
+}
